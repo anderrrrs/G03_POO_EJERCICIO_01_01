7b6ca6a5adc8dcbd186c2c0201ec01080f7ae231
USER <USER@172.16.218.223>
2022-04-01 08:12:50 -0500

    EJERCICIO 01.01 ANDRES MEJIA Y JONATHAN CORONEL

--- src/main/java/com/mycompany/ejercicio01/Principal.java
+++ src/main/java/com/mycompany/ejercicio01/Principal.java
@@ -20,42 +20,68 @@ public class Principal {
         
     ciudad.nombre = "Cuenca";
     ciudad.provincia = "Azuay";
-    
+    ciudad.nombre = "Loja";
+    ciudad.provincia = "Azuay";
+
         
     System.out.println("El nombre de la ciudad es: " +ciudad.nombre);
     System.out.println("El nombre de la provincia es: " +ciudad.provincia);
-    
+    System.out.println("El nombre de la ciudad es: " +ciudad.nombre);
+    System.out.println("El nombre de la provincia es: " +ciudad.provincia);
+    System.out.println("----------------------------------");
+ 
         var clientes = new Clientes();
         
-    clientes.nombre = "Jose";
+    clientes.nombre = "Jonathan";
     clientes.apellido = "Coronel";
+    clientes.nombre = "Jose";
+    clientes.apellido = "Carpio";
     
     System.out.println("El nombre es: " +clientes.nombre);
     System.out.println("El apellido es: " +clientes.apellido);
-    
+    System.out.println("El nombre es: " +clientes.nombre);
+    System.out.println("El apellido es: " +clientes.apellido);
+    System.out.println("----------------------------------");
+
         var servicio = new Servicio();
         
     servicio.nombreTrabajor = "Juan";
     servicio.direccion = "Baños";
+    servicio.nombreTrabajor = "Pablo";
+    servicio.direccion = "Jimenez";
+
     
     System.out.println("El nombre es: " +servicio.nombreTrabajor);
     System.out.println("La direccion es: " +servicio.direccion );
-    
+    System.out.println("El nombre es: " +servicio.nombreTrabajor);
+    System.out.println("La direccion es: " +servicio.direccion );
+    System.out.println("----------------------------------");
+
        var trabajador = new Trabajador();
        
     trabajador.nombre = "Andres";
     trabajador.apellido = "Plaza";
-    
+    trabajador.nombre = "Sofia";
+    trabajador.apellido = "Reyes";
+
         System.out.println("El nombre es: " +trabajador.nombre);
         System.out.println("El apellido es: " +trabajador.apellido);
-        
+        System.out.println("El nombre es: " +trabajador.nombre);
+        System.out.println("El apellido es: " +trabajador.apellido);
+        System.out.println("----------------------------------");
+
        var vehiculos = new Vehiculos();
        
     vehiculos.placa = "ABC-342";
     vehiculos.modelo = "Chevrolet";
-    
+    vehiculos.placa = "BAM-322";
+    vehiculos.modelo = "Nissan";
+
         System.out.println("la placa es: " +vehiculos.placa);
         System.out.println("El modelo es: " +vehiculos.modelo);
+        System.out.println("la placa es: " +vehiculos.placa);
+        System.out.println("El modelo es: " +vehiculos.modelo);
+        System.out.println("----------------------------------");
 
         
         
